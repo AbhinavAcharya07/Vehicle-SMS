77dccf6bba02bcbbd61a2a8d8813663ea7b3f791
Abhinav <abhinavacharya707@gmail.com>
2026-06-22 23:45:42 +0530

    fix:app.listen1

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors({ 
   origin: [
     'http://localhost:5173', 
-    'https://vehicle-sms-a3ll.vercel.app/' 
+    'https://vehicle-sms-a3ll.vercel.app' 
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
